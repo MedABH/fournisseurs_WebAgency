dba18433a81923c3a4521ea71b807b22725af0ef
lamtigui <anasslamtigui9@gmail.com>
2025-03-12 12:41:55 +0000

    .

--- resources/views/myApp/admin/links/prospects.blade.php
+++ resources/views/myApp/admin/links/prospects.blade.php
@@ -480,17 +480,18 @@
                                         @csrf
                                         <div class="modal fade" id="remark-{{ $prospect->id }}" tabindex="-1"
                                             aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                            <div class="modal-dialog">
+                                            <div class="modal-dialog modal-dialog-centered">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
 
+                                                        <h5 class="modal-title" id="exampleModalLabel">REMARQUE</h5>
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                             aria-label="Close"></button>
                                                     </div>
                                                     <div class="modal-body">
 
                                                         <div class="form-group">
-                                                            <label for="remarque">Remarque</label>
+                                                            
                                                             <textarea name="remark" id="remarque" class="form-control" rows="4">{{ old('remark', $prospect->remark) }}</textarea>
                                                             @error('remark')
                                                                 <div class="alert alert-danger">
