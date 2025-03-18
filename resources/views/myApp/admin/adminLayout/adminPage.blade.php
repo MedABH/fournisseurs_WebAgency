23ebaefaceee5e79a1738d054120f0cc1d5e1582
MedABH <128604732+MedABH@users.noreply.github.com>
2025-03-18 12:58:47 +0000

    .

--- resources/views/myApp/admin/adminLayout/adminPage.blade.php
+++ resources/views/myApp/admin/adminLayout/adminPage.blade.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  
     <!--Ce que j'ai ajouté-->
     <link rel="stylesheet" href="{{asset('assets/css/portal.css')}}" />
     <link rel="stylesheet" href="{{asset('assets/css/templatemo-style.css')}}" />
@@ -14,8 +13,6 @@
     <script src="assets/plugins/popper.min.js"></script>
     <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
     -->
-    
-
     <title>@yield('title')</title>
 </head>
 <body>
@@ -23,7 +20,6 @@
     <div class="app-sidepanel-admin">
     @include('myApp.admin.adminLayout.NavSideBar')
     </div>
-
     <div class="content-wrapper">
     @yield('search-bar')
     @yield('parties-prenantes')
