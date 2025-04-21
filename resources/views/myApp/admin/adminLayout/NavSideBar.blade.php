44ac3da9b46677a7dbfd2390b2983445e44a9fe6
lamtigui <anasslamtigui9@gmail.com>
2025-04-21 15:03:54 +0000

    Update NavSideBar.blade.php

--- resources/views/myApp/admin/adminLayout/NavSideBar.blade.php
+++ resources/views/myApp/admin/adminLayout/NavSideBar.blade.php
@@ -64,7 +64,7 @@
                             <div class="app-utility-item app-user-dropdown dropdown">
                                 <a class="dropdown-toggle" id="user-dropdown-toggle" data-bs-toggle="dropdown"
                                     href="#" role="button" aria-expanded="false"><img src="assets/img/logo.png"
-                                        alt="user profile"></a>
+                                        alt=""></a>
                                 <ul class="dropdown-menu" aria-labelledby="user-dropdown-toggle">
                                     <li><a class="dropdown-item" href="{{ route('update.user.auth.form') }}">Compte</a>
                                     </li>
