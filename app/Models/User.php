8929fd0cba3a854286a10aef033ffba32f57413e
lamtigui <anasslamtigui9@gmail.com>
2025-04-21 15:00:36 +0000

    J'ajoute la permission pour les tables

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,6 +60,11 @@ class User extends Authenticatable
     {
         return $this->hasMany(Historique::class);
     }
+    public function permission()
+{
+    return $this->hasOne(Permission::class);
+}
+
     /**
      * The attributes that should be hidden for serialization.
      *
